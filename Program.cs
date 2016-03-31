c50077ede5f1f5d01fe2e41c11546ce7eee72879
Alexander Yarigin <a.yarigin@poisk-it.ru>
2016-03-31 17:09:22 +0300

    Refactor. Add files queue. Add normal destribution by date of adding,

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.IO;
 using SettingsModule;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace RandomMusic
 {
@@ -17,14 +19,18 @@ namespace RandomMusic
             SettingsStorage settings = new SettingsStorage(new XmlSettingsProvider(true));
 
             var musicDirectory = (settings.GetSetting("MusicFromDirectory").GetValue() ?? @"F:\Музыка").ToString();
+
             var musicCopyDirectory = (settings.GetSetting("MusicToDirectory").GetValue() ?? @"E:\Music").ToString();
 
             Console.WriteLine("Путь до папки с музыкой на компьютере:({0})", musicDirectory);
+            SendKeys.SendWait(musicDirectory);
+
             var currentInput = Console.ReadLine();
             if (!String.IsNullOrWhiteSpace(currentInput))
             {
                 musicDirectory = currentInput;
             }
+
 
             var currentDrive = DriveInfo.GetDrives().FirstOrDefault(e => e.Name == musicDirectory.Substring(0, 3));
             if (currentDrive == null)
@@ -42,6 +48,8 @@ namespace RandomMusic
                 }
 
                 Console.WriteLine("Путь до папки с музыкой на мобиле:({0})", musicCopyDirectory);
+                SendKeys.SendWait(musicCopyDirectory);
+
                 currentInput = Console.ReadLine();
                 if (!String.IsNullOrWhiteSpace(currentInput))
                 {
@@ -70,6 +78,8 @@ namespace RandomMusic
                     if (currentFiles.Count > 0)
                     {
                         Console.WriteLine("Папка не пуста. Очистить? (0-нет;1-да)");
+                        SendKeys.SendWait("1");
+
                         var clean = Convert.ToInt32(Console.ReadLine()) == 1;
                         if (clean)
                         {
@@ -85,24 +95,31 @@ namespace RandomMusic
                     Console.WriteLine("Считываем файлы из папки на компьютере...");
 
                     var files = Directory.GetFiles(musicDirectory, "*.*", SearchOption.AllDirectories).Where(e =>
-                                                                                                                 {
-                                                                                                                     var ext = Path.GetExtension(e) ?? "".ToLower();
-                                                                                                                     var fileName = Path.GetFileName(e);
-                                                                                                                     return !currentFiles.Contains(fileName) &&
-                                                                                                                     (ext == ".mp3" || ext == ".flac" || ext == ".ogg" || ext == ".wav");
-                                                                                                                 }).ToList();
+                    {
+                        var maxLength = e.Length > 249 ? 249 : e.Length;
+                        var shortedPath = e.Substring(0, maxLength);
+                        var ext = Path.GetExtension(shortedPath) ?? "".ToLower();
+                        var fileName = Path.GetFileName(shortedPath);
+                        return !currentFiles.Contains(fileName) && (ext == ".mp3" || ext == ".flac" || ext == ".ogg" || ext == ".wav");
+                    }).OrderByDescending(e => new FileInfo(e).LastWriteTimeUtc).ToList();
 
                     Console.WriteLine("Проиндексировать названия песен?(0-нет;1-да)");
+                    SendKeys.SendWait("1");
                     var rename = Convert.ToInt32(Console.ReadLine()) == 1;
 
                     var size = GetDirectorySize(musicCopyDirectory) / (1024 * 1024);
-                    Console.WriteLine("Текущий размер папки:{0:F2} Мб", size);
+                    //Console.WriteLine("Текущий размер папки:{0:F2} Мб", size);
 
                     var freeSpace = currentDrive.TotalFreeSpace / (1024 * 1024);
-                    Console.WriteLine("Максимальный размер папки:{0:F2} Мб", freeSpace + size);
+                    Console.WriteLine("Доступно места:{0:F2} Мб", freeSpace + size);
 
                     Console.WriteLine("Введите желаемый размер папки (Мб)");
                     Int64 maxSize = Convert.ToInt64(Console.ReadLine()) * (1024 * 1024);
+
+                    Console.WriteLine("Выбирать в первую очередь из ранее добавленных? (0-нет;1-да)");
+                    SendKeys.SendWait("1");
+                    var asNormal = Convert.ToInt32(Console.ReadLine())==1;
+
 
                     var wantToHold = maxSize - size * (1024 * 1024);
 
@@ -111,8 +128,9 @@ namespace RandomMusic
 
                     FileCopyState state = new FileCopyState()
                     {
-                        Count = 1,
+                        Count = 0,
                         CurrentSize = size,
+                        AsNormal = asNormal,
                         Random = rnd,
                         Files = filesList,
                         MaxLength = maxSize,
@@ -120,31 +138,45 @@ namespace RandomMusic
                         CopyDirectory = musicCopyDirectory
                     };
 
-                    var selectedFile = SelectFile(state);
-
-                    while (state.CurrentSize < state.MaxLength && state.Files.Count > 0)
+                    
+                    Queue<Tuple<String,String,long,int>> filesQueue = new Queue<Tuple<string, string,long,int>>();
+                    Boolean fillEnds = false;
+                    Task.Run(() =>
                     {
-                        try
+                        while (state.Files.Count > 0)
                         {
-                            var selectTask = Task.Run(() =>
+                            var file = SelectFile(state);
+                            if (file != null&&file.Item2!=null)
                             {
-                                return SelectFile(state);
-                            });
+                                filesQueue.Enqueue(file);
+                            }
 
-                            File.Copy(selectedFile.Item1, selectedFile.Item2, true);
-
-                            Console.WriteLine("{1}. {0} скопирован.", Path.GetFileName(selectedFile.Item2), state.Count++);
-
-                            Console.WriteLine("Прогресс:{0:P1}.", (1 - (state.MaxLength - state.CurrentSize) / (Double)wantToHold));
-                            state.Count++;
-                            state.Files.Remove(selectedFile.Item1);
-                            selectedFile = selectTask.Result;
-                            if (selectedFile==null) break;
                         }
-                        catch (Exception)
+                        fillEnds = true;
+                    });
+                    long summSize = 0;
+                    while (fillEnds&&filesQueue.Count > 0||!fillEnds)
+                    {
+                        if (filesQueue.Count > 0)
                         {
-                            Console.WriteLine("{0} не скопирован", Path.GetFileName(selectedFile.Item2));
-                            state.Files.Remove(selectedFile.Item1);
+                            var selectedFile = filesQueue.Dequeue();
+
+                            try
+                            {
+                                File.Copy(selectedFile.Item1, selectedFile.Item2, true);
+
+                                summSize += selectedFile.Item3;
+                                Console.WriteLine("{1}. {0} скопирован.", Path.GetFileName(selectedFile.Item2),
+                                    selectedFile.Item4);
+
+                                Console.WriteLine("Прогресс:{0:P1}.",
+                                    (1 - (state.MaxLength - summSize)/(Double)wantToHold));
+
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("{0} не скопирован", Path.GetFileName(selectedFile.Item2));
+                            }
                         }
                     }
                     Console.WriteLine("Закончено");
@@ -162,32 +194,51 @@ namespace RandomMusic
             public Int64 MaxLength;
             public Int32 Count;
             public string CopyDirectory;
+            public bool AsNormal { get; set; }
         }
 
-        private static Tuple<String, String> SelectFile(FileCopyState state)
+        private static Tuple<String, String,long,int> SelectFile(FileCopyState state)
         {
             if (state.Files.Count == 0) return null;
-            var index = state.Random.Next(0, state.Files.Count-1);
+            
+            int index;
+            if (state.AsNormal)
+            {
+                double u1 = state.Random.NextDouble(); //these are uniform(0,1) random doubles
+                double u2 = state.Random.NextDouble();
+                double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+                double randNormal = Math.Abs(randStdNormal*0.1)/5;
+                if (randNormal > 1)
+                    randNormal = 1;
+
+                index = (int) (randNormal*(state.Files.Count - 1));
+                Debug.WriteLine(index);
+            }
+            else
+            {
+                index = state.Random.Next(0, state.Files.Count - 1);
+            }
             var file = state.Files.ElementAt(index);
 
             var fileName = Path.GetFileName(file);
             if (fileName.Length >= 250)
             {
                 state.Files.Remove(file);
-                return SelectFile(state);
+                return new Tuple<string, string, long,int>(file, null,0,0);
             }
 
             var fileSize = GetFileSize(file);
             if (state.Rename)
-                fileName = state.Count + "-" + fileName;
+                fileName = (state.Count+1) + "-" + fileName;
             if (state.CurrentSize + fileSize > state.MaxLength)
             {
                 state.Files.Remove(file);
-                return SelectFile(state);
+                return new Tuple<string, string, long, int>(file, null, 0,0);
             }
-
             state.CurrentSize += fileSize;
-            return new Tuple<string, string>(file, state.CopyDirectory + @"\" + fileName);
+            state.Count++;
+            state.Files.Remove(file);
+            return new Tuple<string, string, long, int>(file, state.CopyDirectory + @"\" + fileName, fileSize, state.Count);
         }
 
         private static long GetDirectorySize(string folderPath)
